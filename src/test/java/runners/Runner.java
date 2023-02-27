ab6b45c874e98302de41ba51b1f9648fb26a757d
Funda <fzelihadogan@gmail.com>
2023-02-26 23:11:01 -0500

    new

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         features = "./src/test/resources/features",
         glue = {"stepdefinitions","hooks"},
         dryRun = false,
-        tags = "@US08"
+        tags = "@US03"
 )
 public class Runner {
 
