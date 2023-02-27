9f824e017887b019c09d687090c0d8683cefa874
YaseminOrman <jazmintunc@gmail.com>
2023-02-26 20:02:49 -0600

    new file

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         features = "./src/test/resources/features",
         glue = {"stepdefinitions","hooks"},
         dryRun = false,
-        tags = "@US03"
+        tags = "@TC_01Login"
 )
 public class Runner {
 
