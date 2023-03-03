c681e1f5bc663cb1523a43c324b006b90663c6e3
Mustafa Babadagi <fathermountain@gmail.com>
2023-03-03 14:23:43 -0500

    first version

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -18,12 +18,7 @@ import org.junit.runner.RunWith;
         features = "./src/test/resources/features",
         glue = {"stepdefinitions","hooks"},
         dryRun = false,
-
-        tags = "@db_validation_email"
-
-
-
+        tags = "@UI_Dr_Appt"
 )
 public class Runner {
-
 }
